a48d4e5aad5468842479a1267329b97bbade9195
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2025-05-15 22:49:32 +0530

    add conditionally rendered stats

--- src/App.js
+++ src/App.js
@@ -69,6 +69,9 @@ function Item({ item , onDeleteItem ,onUpdateItem}) {
 }
 
 function Stats({ items }) {
+    if(!items.length) return <p className='stats'>
+        <em>Start adding some items to your packing list 🚀</em>
+    </p>
     const total = items.length;
     const packed = items.filter((item) => item.packed).length;
     const percent = total > 0 ? Math.round((packed / total) * 100) : 0;
@@ -76,11 +79,14 @@ function Stats({ items }) {
     return (
         <footer className="stats">
             <em>
-                You have {total} item{total !== 1 ? 's' : ''} on your list, and you already packed {packed} ({percent}%)
+                {percent === 100
+                    ? "You got everything packed! 🎉 Happy travels ✈️"
+                    : `You have ${total} item${total !== 1 ? 's' : ''} on your list 📃, and you already packed ${packed} ✅ (${percent}%)`}
             </em>
         </footer>
     );
 }
+
 
 function App() {
     const [items, setItems] = useState([]);
